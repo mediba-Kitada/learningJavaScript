d35482b767459bd7a714cf6ca00a358720179beb
北田 翼 <kitada@mediba.jp>
2017-03-14 10:11:21 +0900

    9章 オブジェクトとオブジェクト指向プログラミング / オブジェクトのリスト処理 take02

--- example/ch09/ex09-01/main.js
+++ example/ch09/ex09-01/main.js
@@ -7,6 +7,26 @@ const o = {
   [SYM]: 4 // シンボルをプロパティに含むためのリテラル表記
 }
 for (let prop in o) {
-  if (!o.hasOwnProperty(prop)) continue
+  if (!o.hasOwnProperty(prop)) continue // 継承によってプロパティをもつ場合があるので、独自のプロパティを列挙したい場合はhasOwnPropertyによるチェックが必要
   console.log(`${prop}: ${o[prop]}`)
 }
+
+const o2 = {
+  a: 1,
+  b: 2,
+  c: 3,
+  SYM: 4 // プロパティのキーは、"SYM"となる
+}
+console.log(o2.SYM);
+console.log(o2["SYM"])
+console.log(o2[SYM]) // undefined
+
+const o3 ={
+  a: 1,
+  b: 2,
+  c: 3
+}
+o3[SYM] =4;
+console.log(o3.SYM)
+console.log(o3["SYM"])
+console.log(o3[SYM])
